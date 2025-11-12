0eae70e40c229aca6cf10a4e3d41521f3d55bdef
Pruthvi2911 <pruthvibijjarapu2911@gmail.com>
2025-11-12 19:33:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,26 @@
-from flask import flask,render_template
-name=Flask(__name__)
+from flask import Flask, render_template
+
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
 @app.route("/education")
-def home():
+def education():
     return render_template("education.html")
 
-
 @app.route("/experience")
-def home():
+def experience():
     return render_template("experience.html")
 
-
 @app.route("/projects")
-def home():
+def projects():
     return render_template("projects.html")
 
-
 @app.route("/writetome")
-def home():
+def writetome():
     return render_template("writetome.html")
 
-
-
-if __name__=='__main__':
+if __name__ == "__main__":
     app.run(debug=True)
-
